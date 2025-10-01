254c47a04c964269a94f8d4ee57c083def7a1591
eto65536 <eyunya1126@gmail.com>
2025-10-01 14:51:02 +0900

    Update OmniESP.ino

--- src/OmniESP.ino
+++ src/OmniESP.ino
@@ -9,6 +9,6 @@ void setup() {
 
 void loop() {
   if (PS4.isConnected()) {
-      Serial.printf("%dX%d\n", PS4.LStickX(),PS4.LStickY());
+      Serial.printf("%dX%dY", PS4.LStickX(),PS4.LStickY());
   }
 }
